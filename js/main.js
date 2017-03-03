eb13e281dbef19c0bd2a2bb3d7af2219911f51ca
Stan1812 <2314980309@QQ.com>
2017-03-03 23:42:06 +0800

    add js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+$(function()
+{
+	'use strict';
+	var sidebar=$('#sidebar'),
+		mask=$(',mask'),
+		sidebar_trigger=$('#sidebar_trigger');
+
+	sidebar_trigger.on('click' ,function()
+		{
+			sidebar.animate({'right';0})
+			
+	})return:fluse;
+
+})
+
+
+
+
+
+	
